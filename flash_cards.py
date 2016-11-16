2b24e893bb138b31dc9c8e7f1ae21332457f1abe
A.Y. Siu <aysiu@users.noreply.github.com>
2016-11-16 12:26:01 -0800

    Comments for workflow

--- flash_cards.py
+++ flash_cards.py
@@ -7,7 +7,20 @@ import random
 #phrases = { "Buenos días.": "Good morning.", "Buenas tardes.": "Good afternoon.", "Buenas noches.": "Good evening. (greeting)", "Hola, me llamo Juan.": "Hello, my name is John.", "¿Cómo se llama usted?": "What is your name?", "¿Cómo está usted?": "How are you?", "Estoy bien.": "I am fine.", "Mucho gusto.": "Nice to meet you.", "Adiós.": "Goodbye.", "Hasta luego.": "See you later.", "Estoy perdido. ¿Dónde está el baño?": "I am lost. Where is the restroom?", "Con permiso. OR Perdóname": "Excuse me.", "Por favor.": "Please.", "Gracías.": "Thank you.", "Lo siento.": "I'm sorry.", "Salud.": "Bless you.", "De nada.": "You are welcome (it was nothing).", "¿Cuánto cuesta?": "How much does it cost?", "¿Cuántos hay?": "How many are there?", "Hay muchos.": "There are many.", "¿Quiere comprarlo usted?": "Do you want to buy this?", "¿Qué hora es?": "What time is it?", "¿Cómo se dice maybe en Español?": "How do you say maybe in Spanish?", "Sí.": "Yes.", "No.": "No.", "Yo no comprendo.": "I do not understand.", "Por favor, habla mas despacio.": "Would you speak slower, please.", "¿Quièn?": "Who?", "¿Por què?": "Why?"}
 
 # Uncomment this line to use Chinese
-phrases = { "歡迎光臨": "Welcome", "你好": "Hello (General greeting)", "好久不見": "Long time no see", "我姓 …": "My name is …", "午安": "Good afternoon", "晚安": "Good night", "再見": "Goodbye", "祝你好運": "Good luck", "乾杯!": "Cheers! Good Health!", "我懂": "I understand", "我不知道": "I don't know", "我不懂": "I don't understand", "這個多少錢?": "How much is this?", "我愛你": "I love you", "我的氣墊船裝滿了鱔魚": "My hovercraft is full of eels"}
+#phrases = { "歡迎光臨": "Welcome", "你好": "Hello (General greeting)", "好久不見": "Long time no see", "我姓 …": "My name is …", "午安": "Good afternoon", "晚安": "Good night", "再見": "Goodbye", "祝你好運": "Good luck", "乾杯!": "Cheers! Good Health!", "我懂": "I understand", "我不知道": "I don't know", "我不懂": "I don't understand", "這個多少錢?": "How much is this?", "我愛你": "I love you", "我的氣墊船裝滿了鱔魚": "My hovercraft is full of eels"}
 
 # Uncomment this line to use French
 #phrases = { "Bonjour": "Hello. (literally: good day)", "Je m'appelle...": "My name is... (literally: I call myself)", "Comment vous appelezvous?": "What is your name?", "Parlez lentement.": "Please speak slowly.", "Je ne comprends pas.": "I don't understand.", "Merci": "Thank you.", "De rien.": "You're welcome", "Excusez-moi.": "Excuse me.", "Je t'aime.": "I love you. (informal)", "Je veux être avec toi.": "I want to be with you.", "Comment allez-vous?": "How are you? (formal or plural)", "Je suis de...": "I am from...", "Je voudrais...": "I would like...", "Salut!": "Hi! / Bye! (informal)", "Ça va?": "How are you? (informal)", "Où est-ce que se trouve...?": "Where is...?", "Combien coûte-t-il?": "How much does it cost?", "S'il vous plaît.": "Please.", "Ne t'en fais pas.": "Don't worry. (informal)", "Ne vous en faites pas.": "Don’t worry. (formal)", "Non.": "No.", "D'accord": "OK", "Je ne comprends pas.": "I don't understand.", "Avez-vous...": "Do you have...", "Parlez-vous anglais?": "Do you speak English?", "Qu'est-ce que vous avez dit?": "What did you say?", "Qu sont les toilettes?": "Where are the bathrooms?", "Ce n'est pas grave.": "No problem.", "Je vais bien.": "I’m fine.", "Allons-y!": "Let’s go!", "Tu parles anglais?": "Do you speak English? (informal)", "Je ne sais pas.": "I don't know.", "Au revoir.": "Goodbye."}
+
+
+# Pick a random phrase
+
+# Display a phrase at random
+
+# Get the user input
+
+# Check the user input against the random phrase
+
+# If it matches, say it matches
+
+# Otherwise, say what the match would have been
