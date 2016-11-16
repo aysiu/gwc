e383f63c1cdb9a371fa358466d4a021c56c7b8d5
A.Y. Siu <aysiu@users.noreply.github.com>
2016-11-16 14:03:22 -0800

    Update flash_cards.py

--- flash_cards.py
+++ flash_cards.py
@@ -13,13 +13,13 @@ import random
 #phrases = { "Bonjour": "Hello. (literally: good day)", "Je m'appelle...": "My name is... (literally: I call myself)", "Comment vous appelezvous?": "What is your name?", "Parlez lentement.": "Please speak slowly.", "Je ne comprends pas.": "I don't understand.", "Merci": "Thank you.", "De rien.": "You're welcome", "Excusez-moi.": "Excuse me.", "Je t'aime.": "I love you. (informal)", "Je veux être avec toi.": "I want to be with you.", "Comment allez-vous?": "How are you? (formal or plural)", "Je suis de...": "I am from...", "Je voudrais...": "I would like...", "Salut!": "Hi! / Bye! (informal)", "Ça va?": "How are you? (informal)", "Où est-ce que se trouve...?": "Where is...?", "Combien coûte-t-il?": "How much does it cost?", "S'il vous plaît.": "Please.", "Ne t'en fais pas.": "Don't worry. (informal)", "Ne vous en faites pas.": "Don’t worry. (formal)", "Non.": "No.", "D'accord": "OK", "Je ne comprends pas.": "I don't understand.", "Avez-vous...": "Do you have...", "Parlez-vous anglais?": "Do you speak English?", "Qu'est-ce que vous avez dit?": "What did you say?", "Qu sont les toilettes?": "Where are the bathrooms?", "Ce n'est pas grave.": "No problem.", "Je vais bien.": "I’m fine.", "Allons-y!": "Let’s go!", "Tu parles anglais?": "Do you speak English? (informal)", "Je ne sais pas.": "I don't know.", "Au revoir.": "Goodbye."}
 
 
-# Pick a random phrase
+# Pick a random phrase to focus on
 
-# Display a phrase at random
+# Display the random phrase
 
-# Get the user input
+# Get the user input on a guess for what the random phrase is in English
 
-# Check the user input against the random phrase
+# Check the user input against the original random phrase
 
 # If it matches, say it matches
 
